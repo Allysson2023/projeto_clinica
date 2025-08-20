dcf5ec7f689dd55088f5e303e8c29dd9bb9035f4
Allysson <allyssoncarlos.ac21@gmail.com>
2025-08-20 19:22:04 -0300

    Trabalhando com Eventos no JavaScript

--- script/index.js
+++ script/index.js
@@ -4,15 +4,15 @@ var fields = document.querySelectorAll("#form-user-create [name] " );
 
 var user = {};
 
-fields.forEach(function( field, index ){
-    //console.log(field);
 
-    user[field.name] = field.value;
-
-});
-console.log(user);
 
 document.getElementById("form-user-create").addEventListener("submit", function( event ){
+    fields.forEach(function( field, index ){
+        //console.log(field);
+    
+        user[field.name] = field.value;
+    
+    });
     // Cancelar o Evento
     event.preventDefault();
     console.log(user);

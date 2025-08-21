2b90a25f7f4b985adb9c2eb473b9dff97afb5aba
Allysson <allyssoncarlos.ac21@gmail.com>
2025-08-21 12:30:56 -0300

    novo arquivo model User.js, para coloca o MVC

---
+++ model/User.js
@@ -0,0 +1,10 @@
+class User{
+
+    constructor(nome, whatsapp, serviço, messagem){
+        this.nome = nome;
+        this.whatsapp = whatsapp;
+        this.serviço = serviço;
+        this.messagem = messagem;
+    }
+
+}
